758857543e6b0342a4900c3d01f7a9d661ab6d0d
samsoncs <samsoncs@stud.ntnu.no>
2017-11-13 19:34:45 +0100

    initial commit

---
+++ Assets/Scripts/ReticleController.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ReticleController : MonoBehaviour {
+
+    private Camera mainCamera;
+
+	// Use this for initialization
+	void Start () {
+        this.mainCamera = FindObjectOfType<Camera>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        transform.LookAt(this.mainCamera.transform.position, Vector3.up);
+        var mousePosition = mainCamera.ScreenToWorldPoint(Input.mousePosition);        
+        transform.position = new Vector3(mousePosition.x, 1, mousePosition.y);
+    }
+}
