f7fe21fb00f65619fbaa36263d7af6517f1bd397
Markusandersson9 <markusandersson9@gmail.com>
2017-11-13 21:34:07 +0100

    First day

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,29 +1,58 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class PlayerController : MonoBehaviour {
+public class PlayerController : MonoBehaviour
+{
 
     public float movementSpeed;
+    public float cooldown;
+    private float cooldownTimer = 0;
     public string horizontalInput; //String used to get input axis for the selected player
     public string verticalInput; //String used to get input axis for the selected player
+    public GameObject fireball;
 
     private Rigidbody body;
     private Vector3 movementInput;
     private Vector3 movementVelocity;
 
-	// Use this for initialization
-	void Start () {
+    // Use this for initialization
+    void Start()
+    {
         this.body = GetComponent<Rigidbody>();
-	}
-	
-	// Update is called once per frame
-	void Update () {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
         this.movementInput = new Vector3(Input.GetAxisRaw(horizontalInput), 0, Input.GetAxisRaw(verticalInput));
         this.movementVelocity = this.movementInput * this.movementSpeed;
-	}
+        if (Input.GetAxisRaw("Fire1") != 0)
+        {
+            castFireball();
+        }
+
+        this.cooldownTimer += Time.deltaTime;
+    }
 
     void FixedUpdate()
     {
         this.body.velocity = this.movementVelocity;
+    }
+
+    void castFireball()
+    {
+
+        if (cooldownTimer > cooldown)
+        {
+
+            var ball = Instantiate(this.fireball, this.transform.position + new Vector3(2, 0, 0), this.transform.rotation);
+            if (ball != null)
+            {
+                var ballscript = ball.GetComponent<Fireball>();
+                ballscript.setDirection(this.body.rotation.eulerAngles);
+
+            }
+            this.cooldownTimer = 0;
+        }
     }
 }
---
+++ Assets/Scripts/Spells/Fireball.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+
+public class Fireball : MonoBehaviour {
+
+    private Rigidbody body;
+
+    public float speed;
+    private Vector3 direction;
+
+    public Fireball(Vector3 direction)
+    {
+        this.direction = direction*speed;
+    }
+
+	// Use this for initialization
+	void Start () {
+        this.body = this.GetComponent<Rigidbody>();
+        //this.body.AddForce(this.direction*speed);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void setDirection(Vector3 direction)
+    {
+        this.direction = direction;
+    }
+
+    void FixedUpdate()
+    {
+        //this.body.velocity = Vector3.forward * speed;
+        transform.Translate(Vector3.forward * speed * 0.01f);
+    }
+}
